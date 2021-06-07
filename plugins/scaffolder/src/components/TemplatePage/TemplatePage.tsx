a4913b52adad62b5cd3696091a2d385464bf216a
blam <ben@blam.sh>
2021-06-07 13:35:13 +0200

    chore: making some more progress with webpack 5
    Signed-off-by: blam <ben@blam.sh>

--- plugins/scaffolder/src/components/TemplatePage/TemplatePage.tsx
+++ plugins/scaffolder/src/components/TemplatePage/TemplatePage.tsx
@@ -184,7 +184,7 @@ export const TemplatePage = ({
         pageTitleOverride="Create a New Component"
         title={
           <>
-            Create a New Component <Lifecycle alpha shorthand />
+            Create a New Thing <Lifecycle alpha shorthand />
           </>
         }
         subtitle="Create new software components using standard templates"
